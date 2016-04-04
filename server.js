cade67f8e451c15b1ed53224942496a85a53bfa0
Matthew Vilhauer <matthewvilhauer@gmail.com>
2016-04-04 09:21:13 -0700

    Added Nathan's code

--- server.js
+++ server.js
@@ -10,6 +10,11 @@ app = express();
 // populate the req.body object
 app.use(bodyParser.urlencoded({ extended: true }));
 
+app.use(function(req, res, next) {
+        res.header("Access-Control-Allow-Origin", "*");
+        res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+        next();
+    });
 
 /**********
  * ROUTES *
